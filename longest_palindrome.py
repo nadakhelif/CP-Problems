ccca825e8e6f45cf2ea9ed00ccab80fabc07d623
nada <nadakhelif61@gmail.com>
2024-04-20 01:19:47 +0100

    longest_palindrome

---
+++ longest_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        longest_palindrome_start, longest_palindrome_len = 0, 1
+
+        for i in range(0, n):
+            right = i
+            while right < n and s[i] == s[right]:
+                right += 1
+           
+            left = i - 1
+            while left >= 0 and right < n and s[left] == s[right]:
+                left -= 1
+                right += 1
+                
+            palindrome_len = right - left - 1
+            if palindrome_len > longest_palindrome_len:
+                longest_palindrome_len = palindrome_len
+                longest_palindrome_start = left + 1
+            
+        return s[longest_palindrome_start: longest_palindrome_start + longest_palindrome_len]
