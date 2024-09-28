c7e95f6d0dfe70dde5e981c239eec0318548cde3
nada <nadakhelif61@gmail.com>
2024-09-28 16:27:01 +0100

    max_watered_sections

---
+++ B problems/PetyaAndCountryside.py
@@ -0,0 +1,27 @@
+def max_watered_sections(n, heights):
+    max_watered = 0
+    
+    for i in range(n):
+        left_count = 0
+        for j in range(i, 0, -1): 
+            if heights[j] >= heights[j-1]:
+                left_count += 1
+            else:
+                break
+        right_count = 0
+        for j in range(i, n-1):
+            if heights[j] >= heights[j+1]:
+                right_count += 1
+            else:
+                break
+
+        total_watered = left_count + right_count + 1
+        max_watered = max(max_watered, total_watered)
+
+    print(max_watered) 
+
+n = int(input())  
+heights = list(map(int, input().split())) 
+
+# Output the result
+max_watered_sections(n, heights)
