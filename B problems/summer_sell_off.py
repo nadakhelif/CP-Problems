fc6ea6133cad8cdb6bed02e36830d88c238b8d24
nada <nadakhelif61@gmail.com>
2024-09-29 21:37:44 +0100

    most common word

--- B problems/summer_sell_off.py
+++ B problems/summer_sell_off.py
@@ -3,7 +3,11 @@ l=[]
 for _ in range(n):
     a,b=map(int,input().split())
     l.append([a,b])
-l.sort(key=lambda y:min(2*y[0],y[1])-min(y[0],y[1]))
+    
+def sellout_profit(y):
+    return min(2 * y[0], y[1]) - min(y[0], y[1])
+
+l.sort(key=sellout_profit)
 s=0
 for i in range(n):
     if n>k:
