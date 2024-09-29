9225bcbdc6749a089cbd031817a0d7c0cde2975b
nada <nadakhelif61@gmail.com>
2024-09-29 17:37:14 +0100

    summer_sell_out

--- B problems/summer_sell_off.py
+++ B problems/summer_sell_off.py
@@ -1,57 +1,16 @@
-def summer_sell_off():
-    i, f = map(int, input().split())
-    products = []
-    clients = []
-    if i == 0:
-        print(0)
-        return
-    if f == 0:
-        total_sum = 0  
-        for _ in range(i):
-            k, l = map(int, input().split())  
-            total_sum += min(k, l)
-        print(total_sum)
-        return  
-    list_of_added_products = [0] * f
-    
-    total_sum = 0  
-    for _ in range(i):
-        k, l = map(int, input().split())
-        products.append(k)
-        clients.append(l)
-    for k in range(f):
-        if clients[k] <= products[k]:
-            total_sum += clients[k]  
-            continue 
-        diff = clients[k] - products[k] * 2 
-        if diff >= 0:  
-            list_of_added_products.remove(0)
-            list_of_added_products.append(products[k])
-            total_sum += products[k]  
-        else:  
-            list_of_added_products.remove(0)
-            list_of_added_products.append(clients[k] - products[k])
-            total_sum += products[k]
-    for j in range(f,i):
-        if clients[j] <= products[j]:
-            total_sum += clients[j]  
-            continue 
-        diff = clients[j] - products[j] * 2  
-        if diff >= 0:  
-            if min(list_of_added_products) < products[j]:
-                min_value = min(list_of_added_products)
-                list_of_added_products.remove(min_value)
-                list_of_added_products.append(products[j])
-            total_sum += products[j]  
-        else:  
-            if min(list_of_added_products) < (clients[j] - products[j]):
-                min_value = min(list_of_added_products)
-                list_of_added_products.remove(min_value)
-                list_of_added_products.append(clients[j] - products[j])
-            total_sum += products[j]  
-    total_sum += sum(list_of_added_products)
-    print(total_sum)  
-    
-        
-    
-summer_sell_off()
+n,k=map(int,input().split())
+l=[]
+for _ in range(n):
+    a,b=map(int,input().split())
+    l.append([a,b])
+l.sort(key=lambda y:min(2*y[0],y[1])-min(y[0],y[1]))
+s=0
+for i in range(n):
+    if n>k:
+        s+=min(l[i][0],l[i][1])
+    else:
+        s+=min(2*(l[i][0]),l[i][1])
+    n-=1
+print(s)        
+ 
+ 
